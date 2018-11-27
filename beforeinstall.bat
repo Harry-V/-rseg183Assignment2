68d8e9b17a9a83f5ec904af42372165a00eb54af
Harry-V <41595599+Harry-V@users.noreply.github.com>
2018-11-27 12:16:46 -0500

    Update beforeinstall.bat

--- beforeinstall.bat
+++ beforeinstall.bat
@@ -1,6 +1,6 @@
  %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "Set-ExecutionPolicy Unrestricted -Force"
 
 
-IF NOT EXIST c:\temp_11 mkdir c:\temp_11 
+IF NOT EXIST c:\temp_11 mkdir c:\temp_11
 cd c:\temp_11
  
